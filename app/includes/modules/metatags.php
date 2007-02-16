97ce1a9a34a2db233585a958279d29bc2559aac6
aep <aep@localhost>
2007-02-16 00:01:08 -0800

    O-017 A0010

--- app/includes/modules/metatags.php
+++ app/includes/modules/metatags.php
@@ -43,78 +43,61 @@ if (strstr($APP_PATH[1], "products"))
         $smarty->assign("HEAD_TITLE",TITLE);
     }
 } 
+else if (strstr($APP_PATH[1], "catalog")) 
+{    global $current_category_id;
+
+    $categories_meta_query = xtDBquery("SELECT categories_meta_keywords,
+                                                categories_meta_description,
+                                                categories_meta_title,
+                                                categories_name
+                                                FROM " . TABLE_CATEGORIES_DESCRIPTION . "
+                                                WHERE categories_id='" .  $current_category_id . "' and
+                                                languages_id='" . $_SESSION['languages_id'] . "'");
+    $categories_meta = xtc_db_fetch_array($categories_meta_query, true);
+    if ($categories_meta['categories_meta_keywords'] == '') 
+    {
+        $categories_meta['categories_meta_keywords'] = META_KEYWORDS;
+    }
+    if ($categories_meta['categories_meta_description'] == '') 
+    {
+        $categories_meta['categories_meta_description'] = META_DESCRIPTION;
+    }
+    if ($categories_meta['categories_meta_title'] == '') 
+    {
+        $categories_meta['categories_meta_title'] = $categories_meta['categories_name'];
+    }
+
+
+    $head[]=array('name'=>"description" ,'content'=>$categories_meta['categories_meta_description']);
+    $head[]=array('name'=>"keywords" ,'content'=>$categories_meta['categories_meta_keywords']);
+    $smarty->assign("HEAD_TITLE",TITLE.' - '.$categories_meta['categories_meta_title']);
+
+} 
 else 
 {
-    if ($_GET['cPath']) 
+    if ($_GET['coID']) 
     {
-        if (strpos($_GET['cPath'], '_') == '1') 
-        {
-            $arr = explode('_', xtc_input_validation($_GET['cPath'], 'cPath', ''));
-            $_cPath = $arr[1];
-        } 
-        else 
-        {   
-            //$_cPath=(int)$_GET['cPath'];
-            if (isset ($_GET['cat'])) 
-            {
-                $site = explode('_', $_GET['cat']);
-                $cID = $site[0];
-                $_cPath = str_replace('c', '', $cID);
-            }
-        }
-        $categories_meta_query = xtDBquery("SELECT categories_meta_keywords,
-                                                    categories_meta_description,
-                                                    categories_meta_title,
-                                                    categories_name
-                                                    FROM " . TABLE_CATEGORIES_DESCRIPTION . "
-                                                    WHERE categories_id='" . $_cPath . "' and
+        $contents_meta_query = xtDBquery("SELECT content_heading
+                                                    FROM " . TABLE_CONTENT_MANAGER . "
+                                                    WHERE content_group='" . $_GET['coID'] . "' and
                                                     languages_id='" . $_SESSION['languages_id'] . "'");
-        $categories_meta = xtc_db_fetch_array($categories_meta_query, true);
-        if ($categories_meta['categories_meta_keywords'] == '') 
-        {
-            $categories_meta['categories_meta_keywords'] = META_KEYWORDS;
-        }
-        if ($categories_meta['categories_meta_description'] == '') 
-        {
-            $categories_meta['categories_meta_description'] = META_DESCRIPTION;
-        }
-        if ($categories_meta['categories_meta_title'] == '') 
-        {
-            $categories_meta['categories_meta_title'] = $categories_meta['categories_name'];
-        }
+        $contents_meta = xtc_db_fetch_array($contents_meta_query, true);
 
+        $head[]=array('name'=>"description" ,'content'=>META_DESCRIPTION);
+        $head[]=array('name'=>"keywords" ,'content'=>META_KEYWORDS);
+        $smarty->assign("HEAD_TITLE",TITLE." - ".$contents_meta['content_heading']);
 
-        $head[]=array('name'=>"description" ,'content'=>$categories_meta['categories_meta_description']);
-        $head[]=array('name'=>"keywords" ,'content'=>$categories_meta['categories_meta_keywords']);
-        $smarty->assign("HEAD_TITLE",TITLE.' - '.$categories_meta['categories_meta_title']);
 
     } 
     else 
     {
-        if ($_GET['coID']) 
-        {
-            $contents_meta_query = xtDBquery("SELECT content_heading
-                                                        FROM " . TABLE_CONTENT_MANAGER . "
-                                                        WHERE content_group='" . $_GET['coID'] . "' and
-                                                        languages_id='" . $_SESSION['languages_id'] . "'");
-            $contents_meta = xtc_db_fetch_array($contents_meta_query, true);
 
-            $head[]=array('name'=>"description" ,'content'=>META_DESCRIPTION);
-            $head[]=array('name'=>"keywords" ,'content'=>META_KEYWORDS);
-            $smarty->assign("HEAD_TITLE",TITLE." - ".$contents_meta['content_heading']);
+        $head[]=array('name'=>"description" ,'content'=>META_DESCRIPTION);
+        $head[]=array('name'=>"keywords" ,'content'=>META_KEYWORDS);
+        $smarty->assign("HEAD_TITLE",TITLE);
 
-
-        } 
-        else 
-        {
-
-            $head[]=array('name'=>"description" ,'content'=>META_DESCRIPTION);
-            $head[]=array('name'=>"keywords" ,'content'=>META_KEYWORDS);
-            $smarty->assign("HEAD_TITLE",TITLE);
-
-        }
     }
-}
+}
 
 $smarty->assign("head_metadata",$head);
 
