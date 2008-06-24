7482f3b7b54fdb75104bad8c75f4828b7f12480a
aep <aep@localhost>
2008-06-24 13:39:06 +0000

    using prototype and script.alicio.us

--- app/includes/javascript/general.js.php
+++ app/includes/javascript/general.js.php
@@ -2,8 +2,5 @@
 $smarty->assign("head_scripts", array
                                 (
                                     "/javascript/spin.js",
-                                    "/javascript/mootools.js",
-                                    "/javascript/backgroundSlider.js",
-                                    "/javascript/slideshow.js"
                                 ));
 ?>
