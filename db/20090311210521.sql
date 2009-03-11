2bf8b1aa5c6cc0341fadac7e7c339f469f6e4a8e
aep <aep@localhost>
2009-03-11 23:36:14 +0100

    aaaarg. i needs autoadd

---
+++ db/20090311210521.sql
@@ -0,0 +1,9 @@
+create table `prices`
+(
+    `prices_id`             integer primary key NOT NULL auto_increment,
+    `customers_status_id`   integer references `customers_status` (`customers_status_id`),
+    `products_id`           integer not null references `products` (`products_id`),
+    `quantity`              integer,
+    `price`                 float
+);
+
