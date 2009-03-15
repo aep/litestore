d4c4793a80f41f793efaf7108efd5a80900a6e98
aep <aep@localhost>
2009-03-15 21:43:43 +0100

    adin box kijasnd

--- app/boxes/admin.php
+++ app/boxes/admin.php
@@ -31,10 +31,20 @@ class BoxAdmin extends AbstractVCBox
     }
     function evaluate()
     {
-        $box_smarty = new smarty;
-        $box_smarty->assign('tpl_path','/templates/'.CURRENT_TEMPLATE.'/');
-        $box_smarty->assign('language', $_SESSION['language']);
-        return $box_smarty->fetch('boxes/box_admin.html');
+
+        if(array_key_exists('customers_status',$_SESSION) && 
+            (array_key_exists('customers_status_id',$_SESSION['customers_status'])) &&
+                ($_SESSION['customers_status']['customers_status_id']==='0' || $_SESSION['customers_status']['customers_status_id']===0))
+        {
+            $box_smarty = new smarty;
+            $box_smarty->assign('tpl_path','/templates/'.CURRENT_TEMPLATE.'/');
+            $box_smarty->assign('language', $_SESSION['language']);
+            return $box_smarty->fetch('boxes/box_admin.html');
+        }
+        else
+        {
+            return '';
+        }
     }
     function metatype()
     {
