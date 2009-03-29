2e64a4fae2d34aaa82284f21dcceaa9f518576b8
aep <aep@localhost>
2009-03-29 10:31:06 +0200

    Adding checkout_success to azrael. google seo crap can now be entered by the user directly into the actual content in azrael

---
+++ db/20090329082210.sql
@@ -0,0 +1,3 @@
+insert into `content`  values (21,1,'{0ab256ea-0000-4000-9827-6556fa1eb9e8}','CheckoutSuccess','CheckoutSuccess',0);
+insert into `content`  values (22,21,'{c21ced16-0000-4000-92c1-69d94afb4933}','CheckoutSuccess','<h1>Vielen dank f&uuml;r Ihren Einkauf</h1>',0);
+
