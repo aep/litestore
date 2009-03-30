ee2f8f20ce24cd2a2ec5018df05a358ceb20dc82
aep <aep@localhost>
2009-03-30 11:29:58 +0200

    blafix

--- app/pub/admin/includes/functions/general.php
+++ app/pub/admin/includes/functions/general.php
@@ -1054,17 +1054,17 @@ function xtc_del_image_file($image) {
 
 function xtc_remove_order($order_id, $restock = false) {
 	if ($restock == 'on') {
-		$order_query = xtc_db_query("select products_id, products_quantity from ".TABLE_ORDERS_PRODUCTS." where orders_id = '".xtc_db_input($order_id)."'");
+		$order_query = xtc_db_query("select products_id, products_quantity from ".TABLE_ORDERS_PRODUCTS." where orders_id = '".(int)($order_id)."'");
 		while ($order = xtc_db_fetch_array($order_query)) {
 			xtc_db_query("update ".TABLE_PRODUCTS." set products_quantity = products_quantity + ".$order['products_quantity'].", products_ordered = products_ordered - ".$order['products_quantity']." where products_id = '".$order['products_id']."'");
 		}
 	}
 
-	xtc_db_query("delete from ".TABLE_ORDERS." where orders_id = '".xtc_db_input($order_id)."'");
-	xtc_db_query("delete from ".TABLE_ORDERS_PRODUCTS." where orders_id = '".xtc_db_input($order_id)."'");
-	xtc_db_query("delete from ".TABLE_ORDERS_PRODUCTS_ATTRIBUTES." where orders_id = '".xtc_db_input($order_id)."'");
-	xtc_db_query("delete from ".TABLE_ORDERS_STATUS_HISTORY." where orders_id = '".xtc_db_input($order_id)."'");
-	xtc_db_query("delete from ".TABLE_ORDERS_TOTAL." where orders_id = '".xtc_db_input($order_id)."'");
+	xtc_db_query("delete from ".TABLE_ORDERS." where orders_id = '".(int)($order_id)."'");
+	xtc_db_query("delete from ".TABLE_ORDERS_PRODUCTS." where orders_id = '".(int)($order_id)."'");
+	xtc_db_query("delete from ".TABLE_ORDERS_PRODUCTS_ATTRIBUTES." where orders_id = '".(int)($order_id)."'");
+	xtc_db_query("delete from ".TABLE_ORDERS_STATUS_HISTORY." where orders_id = '".(int)($order_id)."'");
+	xtc_db_query("delete from ".TABLE_ORDERS_TOTAL." where orders_id = '".(int)($order_id)."'");
 }
 
 function xtc_reset_cache_block($cache_block) {
