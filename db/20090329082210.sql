7effb04aa7fccf1b17668ad037f23ac13ca714a0
aep <aep@localhost>
2009-03-30 23:29:55 +0200

    qojnd no one is ever going to read this anyway so blaaaaaaaaaaaaaaa

--- db/20090329082210.sql
+++ db/20090329082210.sql
@@ -1,3 +1,6 @@
-insert into `content`  values (21,1,'{0ab256ea-0000-4000-9827-6556fa1eb9e8}','CheckoutSuccess','CheckoutSuccess',0);
-insert into `content`  values (22,21,'{c21ced16-0000-4000-92c1-69d94afb4933}','CheckoutSuccess','<h1>Vielen dank f&uuml;r Ihren Einkauf</h1>',0);
+insert into `content`  values (21,1,'{0ab256ea-0000-4000-9827-6556fa1eb9e8}','Checkout','Checkout',0);
+insert into `content`  values (22,21,'{0ab256ea-0000-4000-9827-6556fa1eb9e8}','CheckoutSuccess','CheckoutSuccess',0);
+insert into `content`  values (23,22,'{c21ced16-0000-4000-92c1-69d94afb4933}','CheckoutSuccess','<h1>Vielen dank f&uuml;r Ihren Einkauf</h1>',0);
+
+
 
