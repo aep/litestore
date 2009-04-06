e685ea25857bbdea108eef84cffde4ae3ee9d742
aep <aep@localhost>
2009-04-06 15:40:38 +0200

    fix migrate

--- db/migrate.php
+++ db/migrate.php
@@ -5,13 +5,28 @@ class DB  extends PDO
 {
     public function __construct()
     {
-        $file = $_SERVER['argv'][1];
+        $file = DIR_FS_USER.'/db/db.ini';
         if (!$settings = parse_ini_file($file, TRUE))
+        { 
             throw new exception('Unable to open ' . $file . '.');
-       
-        $dns = $settings['database']['driver'] . ':host=' . $settings['database']['host'] . 
-        ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
-        ';dbname=' . $settings['database']['schema'];
+        }
+
+        $dns='';       
+        if($settings['database']['driver']=='sqlite')
+        {
+            if($settings['database']['file'][0]!='/')
+            {
+                $settings['database']['file']=DIR_FS_USER.'/db/'.$settings['database']['file'];
+            }
+            $dns = $settings['database']['driver'] . ':' . $settings['database']['file'];
+        }
+        else
+        {
+            $dns = $settings['database']['driver'] . ':host=' . $settings['database']['host'] . 
+            ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
+            ';dbname=' . $settings['database']['schema'];
+        }
+        
        
         parent::__construct($dns, $settings['database']['username'], $settings['database']['password']);
 
