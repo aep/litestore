d88e68ba0b467f1cdaf6c36a7d6d63f682c6fa66
aep <aep@localhost.localdomain>
2009-06-16 15:24:26 -0700

    stuff

--- app/modules/catalog.php
+++ app/modules/catalog.php
@@ -29,9 +29,8 @@ function module()
     //no such category
     if(!$mq)
     {
-        header("HTTP/1.0 404 Not Found");
-        die('<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN"><html><head><title>404 Not Found</title></head><body><h1>Not Found</h1>
-            <p>The requested URL '.$_GET["path"].' was not found on this server.</p></body></html>');
+        header("Location: ".SEO_NOTFOUNDPAGE);
+        die();
     }
 
 
---
+++ db/20090616214052.sql
@@ -0,0 +1,3 @@
+update configuration set `configuration_value`='false' where `configuration_key`='ACCOUNT_STATE';
+update configuration set `configuration_value`='false' where `configuration_key`='ACCOUNT_DOB';
+
