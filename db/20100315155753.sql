0ba230ecae3cef4e3d9c0c0c9650d9b1758bb5ee
aep <aep@localhost>
2010-06-25 13:54:33 +0200

    correct drop foo

--- db/20100315155753.sql
+++ db/20100315155753.sql
@@ -4,9 +4,8 @@ alter table prices drop column customers_status_id;
 
 alter table prices add column price_group_id int unique;
 alter table prices add column show_old_price decimal(15,4);
+alter table prices drop index `price_group_id`
 alter table prices add unique (products_id,price_group_id,quantity);
-alter table prices drop unique;
-
 
 CREATE TABLE `customers_status_price_group` (
   `customers_status_id` int(11) NOT NULL DEFAULT '0',
